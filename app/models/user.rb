f8574775fa0d17a8268580e2e443db258b3fb379
Michael Vitale <mmvitale@gmail.com>
2016-03-23 10:54:19 -0400

    basic user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :user_name, presence: true
+  validates :email, presence: true
+  validates :full_name, presence: true
+
   has_secure_password
 end
--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -3,9 +3,49 @@ require "rails_helper"
 describe User do
   describe "valid user" do
     it "should be valid" do
-      user = create(:user)
+      user = build(:user)
       expect(user).to be_valid
     end
+  end
+
+  it "must have a password with at least 8 characters" do
+    user = build(:user)
+
+    user.password = user.password_confirmation = nil
+    expect(user).to be_invalid
+
+    user.password = user.password_confirmation = "1234567"
+    expect(user).to be_invalid
+  end
+
+  it "must have a user_name" do
+    user = build(:user)
+    
+    user.user_name = nil
+    expect(user).to be_invalid
+
+    user.user_name = ""
+    expect(user).to be_invalid
+  end
+
+  it "must have an email" do
+    user = build(:user)
+
+    user.email = nil
+    expect(user).to be_invalid
+
+    user.email = ""
+    expect(user).to be_invalid
+  end
+
+  it "must have a full_name" do
+    user = build(:user)
+
+    user.full_name = nil
+    expect(user).to be_invalid
+
+    user.full_name = ""
+    expect(user).to be_invalid
   end
 end
 

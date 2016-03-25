1dc2cfcd9fd046f3ea7a3008d083c7016ace2724
Michael Vitale <mmvitale@gmail.com>
2016-03-25 17:53:38 -0400

    email config using figaro

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,9 +12,21 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # smtp config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    user_name:            "eol.learning",
+    password:             ENV["email_password"],
+    authentication:       "plain",
+    enable_starttls_auto: true,
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log

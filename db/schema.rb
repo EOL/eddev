e160cf2bd2b15c43b5872f10a2851754b0d8f99e
Michael Vitale <mmvitale@gmail.com>
2016-03-28 13:57:57 -0400

    add indices to unique user columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324174446) do
+ActiveRecord::Schema.define(version: 20160328175533) do
 
   create_table "legacy_users", force: :cascade do |t|
     t.string   "user_name",  limit: 255
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 20160324174446) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["user_name"], name: "index_users_on_user_name", unique: true, using: :btree
 
   add_foreign_key "user_migration_invitations", "legacy_users"
 end

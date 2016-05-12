16a24ef63bd123471101c066c3b2839adad41cd2
Michael Vitale <mmvitale@gmail.com>
2016-05-12 16:06:02 -0400

    add habitats to place form

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -2,4 +2,12 @@ module PlacesHelper
   def h1_key(place)
     return "places_h1_#{@place.id}"
   end
+
+  def habitats_for_select
+    Habitat.all.collect { |h| [h.name, h.id] }
+  end
+
+  def habitat_ids(place)
+    place.habitats.collect { |h| h.id } 
+  end
 end

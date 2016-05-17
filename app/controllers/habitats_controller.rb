8320643f6a74f600c59c2ec343ce8df9574d72c7
Michael Vitale <mmvitale@gmail.com>
2016-05-17 13:42:25 -0400

    update routes, controller, views to reflect place ownership of habitats

--- app/controllers/habitats_controller.rb
+++ app/controllers/habitats_controller.rb
@@ -1,63 +1,54 @@
 class HabitatsController < ApplicationController
   before_action :set_habitat, only: [:show, :edit, :update, :destroy]
+  before_action :set_place
 
-  # GET /habitats
-  # GET /habitats.json
+  # GET /places/1/habitats
   def index
-    @habitats = Habitat.all
+    @habitats = Habitat.where(place: @place)
   end
 
-  # GET /habitats/1
-  # GET /habitats/1.json
+  # GET /places/1/habitats/1
   def show
   end
 
-  # GET /habitats/new
+  # GET /places/1/habitats/new
   def new
-    @habitat = Habitat.new
+    @habitat = Habitat.new(place: @place)
   end
 
-  # GET /habitats/1/edit
+  # GET /places/1/habitats/1/edit
   def edit
   end
 
-  # POST /habitats
-  # POST /habitats.json
+  # POST /places/1/habitats
   def create
     @habitat = Habitat.new(habitat_params)
 
     respond_to do |format|
       if @habitat.save
-        format.html { redirect_to @habitat, notice: 'Habitat was successfully created.' }
-        format.json { render :show, status: :created, location: @habitat }
+        format.html { redirect_to place_habitat_path(@place, @habitat), notice: 'Habitat was successfully created.' }
       else
         format.html { render :new }
-        format.json { render json: @habitat.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /habitats/1
-  # PATCH/PUT /habitats/1.json
+  # PATCH/PUT /places/1/habitats/1
   def update
     respond_to do |format|
       if @habitat.update(habitat_params)
-        format.html { redirect_to @habitat, notice: 'Habitat was successfully updated.' }
-        format.json { render :show, status: :ok, location: @habitat }
+        format.html { redirect_to place_habitat_path(@place, @habitat), notice: 'Habitat was successfully updated.' }
       else
         format.html { render :edit }
-        format.json { render json: @habitat.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /habitats/1
-  # DELETE /habitats/1.json
+  # DELETE /places/1/habitats/1
   def destroy
     @habitat.destroy
     respond_to do |format|
-      format.html { redirect_to habitats_url, notice: 'Habitat was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to place_habitats_path(@place), notice: 'Habitat was successfully destroyed.' }
     end
   end
 
@@ -67,8 +58,12 @@ class HabitatsController < ApplicationController
       @habitat = Habitat.find(params[:id])
     end
 
+    def set_place
+      @place = Place.find(params[:place_id])
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def habitat_params
-      params.require(:habitat).permit(:name)
+      params.require(:habitat).permit(:name, :place_id)
     end
 end

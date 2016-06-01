545db7d97da6906c8231d2aaba0a6433180e219a
Michael Vitale <mmvitale@gmail.com>
2016-06-01 15:10:42 -0400

    ContentModelState tests and validations

--- app/models/content_model_state.rb
+++ app/models/content_model_state.rb
@@ -2,6 +2,14 @@ class ContentModelState < ActiveRecord::Base
   belongs_to :content_model, polymorphic: true
 
   validates_presence_of :content_model
-  validates_presence_of :published
+  validates :published, :inclusion => { :in => [true, false] }
   validates_presence_of :editor_content_version
+
+  after_initialize :set_defaults, :unless => :persisted?
+
+  private
+  def set_defaults
+    self.published = false if published.nil?
+    self.editor_content_version = 0 if editor_content_version.nil?
+  end
 end
--- spec/models/content_model_state_spec.rb
+++ spec/models/content_model_state_spec.rb
@@ -2,6 +2,34 @@ require 'rails_helper'
 
 RSpec.describe ContentModelState, type: :model do
   it { should validate_presence_of :content_model }
-  it { should validate_presence_of :published }
   it { should validate_presence_of :editor_content_version }
+
+  describe "default attribute values" do 
+    let(:content_model) { build_stubbed(:habitat) }
+    let(:state) { ContentModelState.new(:content_model => content_model) }
+
+    it "sets defaults so that it is valid" do
+      expect(state).to be_valid
+    end
+
+    it "sets published to false" do
+      expect(state.published).to eq(false)
+    end
+
+    it "sets editor_content_version to 0" do
+      expect(state.editor_content_version).to eq(0)
+    end
+
+    it "doesn't set defaults if it has been persisted" do
+      state.save
+      state.published = nil
+      expect(state).to be_invalid
+
+      state.published = true
+      expect(state).to be_valid
+
+      state.editor_content_version = nil
+      expect(state).to be_invalid
+    end
+  end
 end

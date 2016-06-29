a6b61c8f6fa92cacbd2c9117cf759a6b2ee12128
Michael Vitale <mmvitale@gmail.com>
2016-06-29 17:09:56 -0400

    fix 500 when password invalid

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,7 +6,6 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-
     user_name = params[:user_session][:user_name]
     password =  params[:user_session][:password]
 
@@ -14,20 +13,22 @@ class UserSessionsController < ApplicationController
 
     if user
       redirect_to { "welcome#index" }
-    elsif !User.find_by(:user_name => user_name)
-      # If there isn't a User with that user_name, check if there is a LegacyUser.
-      # If so, generate a UserMigrationInvitation and notify the user.
-      legacy_user = LegacyUser.find_by(:user_name => user_name)
-     
-      if legacy_user
-        # Create invitation    
-        invitation = UserMigrationInvitation.create!(:legacy_user => legacy_user) 
-        invitation_email = UserMigrationInvitationMailer.invitation_email(invitation) 
-        invitation_email.deliver_now
+    else
+      @msg = "Invalid username/password"
 
-        @msg = "Check your email for a link to change your password"
-      else
-        @msg = "Invalid username/password"
+      if !User.find_by(:user_name => user_name)
+        # If there isn't a User with that user_name, check if there is a LegacyUser.
+        # If so, generate a UserMigrationInvitation and notify the user.
+        legacy_user = LegacyUser.find_by(:user_name => user_name)
+       
+        if legacy_user
+          # Create invitation    
+          invitation = UserMigrationInvitation.create!(:legacy_user => legacy_user) 
+          invitation_email = UserMigrationInvitationMailer.invitation_email(invitation) 
+          invitation_email.deliver_now
+
+          @msg = "Check your email for a link to change your password"
+        end 
       end
 
       render :new

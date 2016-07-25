7f6bda520ba4555fb9f8137bdefad5f06f2b017d
Michael Vitale <mmvitale@gmail.com>
2016-07-25 17:17:39 -0400

    more translations, cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
   # GET /users/new
   def new
     @user = User.new
-    render "new"
   end
 
   # POST /users
--- app/mailers/signup_confirmation_mailer.rb
+++ app/mailers/signup_confirmation_mailer.rb
@@ -2,6 +2,6 @@ class SignupConfirmationMailer < ApplicationMailer
   def confirmation_email(user)
     @user = user
     @confirm_url = users_confirm_url @user.confirm_token
-    mail(to: @user.email, subject: 'Complete your EOL L&E registration')
+    mail(:to => @user.email, :subject => t(".subject"))
   end
 end

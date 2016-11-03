3f9edf5f333155df3da5d89dee6885453ff0e63d
Michael Vitale <mmvitale@gmail.com>
2016-11-03 10:55:21 -0400

    about page, refactor intro content

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,11 +1,4 @@
 $(function() {
-  function fixImageTextBars() {
-    $('.nav-item .bar').each(function(i, item) {
-      console.log('scrollWidth: ' + item.scrollWidth);
-      console.log('innerWidth: ' + $(item).innerWidth());
-    });
-  }
-
   function initSlideshow() {
     $('#Slideshow').slick({
       arrows: false,
@@ -16,29 +9,5 @@ $(function() {
     });
   }
 
-  function expandWelcomeBox() {
-    var $box = $('#WelcomeBox'),
-        $hidden = $('#WelcomeContent .hidden'),
-        $moreBar = $('#WelcomeMoreBar'),
-        $moreText = $('#WelcomeMoreText');
-
-    $box.off('mouseenter');
-    
-    $hidden.slideDown(function() {
-      $moreText.hide(); 
-
-      $box.mouseleave(function() { 
-        $box.off('mouseleave');
-
-        $hidden.slideUp(function() {
-          $moreText.show();
-          $box.mouseenter(expandWelcomeBox);
-        });  
-      });
-    });
-  }
-
-  fixImageTextBars();
   initSlideshow();
-  $('#WelcomeBox').mouseenter(expandWelcomeBox);
 });

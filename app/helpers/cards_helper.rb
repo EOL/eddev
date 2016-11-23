25631d47ead279d467c386f94a6204a5242ddd80
Michael Vitale <mmvitale@gmail.com>
2016-11-23 14:55:54 -0500

    update cards page layout

---
+++ app/helpers/cards_helper.rb
@@ -0,0 +1,39 @@
+module CardsHelper
+  ALL_DECKS = [
+  {
+    :text_key   => "bioblitz",
+    :image_name => "bioblitz"
+  },
+  {
+    :text_key   => "bioblitz",
+    :image_name => "okaloosa"
+  },
+  {
+    :text_key   => "bioblitz",
+    :image_name => "new_england"
+  },
+  {
+    :text_key   => "bioblitz",
+    :image_name => "earthwatch"
+  },
+  {
+    :text_key   => "bioblitz",
+    :image_name => "bioblitz"
+  },
+  {
+    :text_key   => "bioblitz",
+    :image_name => "okaloosa"
+  },
+  {
+    :text_key   => "bioblitz",
+    :image_name => "new_england"
+  },
+  {
+    :text_key   => "bioblitz",
+    :image_name => "earthwatch"
+  }]
+
+  def all_decks
+    ALL_DECKS
+  end
+end

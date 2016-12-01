75e0d5b62ede9322b45dd53c5e1fb6908074e400
Michael Vitale <mmvitale@gmail.com>
2016-12-01 15:00:57 -0500

    some card blurbs

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,37 +1,40 @@
 module CardsHelper
   ALL_DECKS = [
   {
-    :text_key   => "bioblitz",
+    :title_key  => "aquatic",
+    :desc_key   => "aquatic",
     :image_name => "bioblitz"
   },
   {
-    :text_key   => "bioblitz",
-    :image_name => "okaloosa"
-  },
-  {
-    :text_key   => "bioblitz",
-    :image_name => "new_england"
-  },
-  {
-    :text_key   => "bioblitz",
-    :image_name => "earthwatch"
-  },
-  {
-    :text_key   => "bioblitz",
+    :title_key => "bioblitz",
+    :sub_key => "common_animal_groups",
+    :desc_key => "bioblitz_common",
     :image_name => "bioblitz"
   },
   {
-    :text_key   => "bioblitz",
-    :image_name => "okaloosa"
+    :title_key => "boston_harbor",
+    :sub_key => "rocky_intertidal",
+    :desc_key => "boston_rocky",
+    :image_name => "bioblitz"
   },
   {
-    :text_key   => "bioblitz",
-    :image_name => "new_england"
+    :title_key => "earthwatch",
+    :sub_key => "california_urban_tree",
+    :desc_key => "earthwatch_cali_tree",
+    :image_name => "bioblitz"
   },
   {
-    :text_key   => "bioblitz",
-    :image_name => "earthwatch"
-  }]
+    :title_key => "ecocolumn",
+    :desc_key => "ecocolumn",
+    :image_name => "bioblitz"
+  },
+  {
+    :title_key => "new_england",
+    :sub_key => "urban_habitat_adaptations",
+    :desc_key => "ne_urban",
+    :image_name => "bioblitz"
+  },
+  ]
 
   def all_decks
     ALL_DECKS

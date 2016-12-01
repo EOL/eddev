d12839ffd13ded95d00b1786f9880aaf26941d91
Michael Vitale <mmvitale@gmail.com>
2016-12-01 15:37:47 -0500

    all species card blurbs (rough draft)

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -34,6 +34,90 @@ module CardsHelper
     :desc_key => "ne_urban",
     :image_name => "bioblitz"
   },
+  {
+    :title_key => "new_england",
+    :sub_key => "vernal_pools",
+    :desc_key => "ne_vernal",
+    :image_name => "bioblitz"
+  },
+  {
+    :title_key => "new_england",
+    :sub_key => "wright_farm",
+    :desc_key => "ne_wright",
+    :image_name => "bioblitz"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "biodiversity",
+    :desc_key => "okaloosa_bio",
+    :image_name => "bioblitz"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "coastal_uplands",
+    :desc_key => "okaloosa_coastal",
+    :image_name => "bioblitz"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "common_birds",
+    :desc_key => "okaloosa_common_birds",
+    :image_name => "bioblitz"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "common_mammals",
+    :desc_key => "okaloosa_common_mammals",
+    :image_name => "bioblitz"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "emammal_camera",
+    :desc_key => "okaloosa_emammal",
+    :image_name => "bioblitz"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "freshwater_forested",
+    :desc_key => "okaloosa_freshwater",
+    :image_name => "bioblitz"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "hardwood_forested",
+    :desc_key => "okaloosa_hardwood",
+    :image_name => "bioblitz"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "natural_adapt",
+    :desc_key => "okaloosa_natural_adapt",
+    :image_name => "bioblitz"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "pines_sandhill",
+    :desc_key => "okaloosa_pines",
+    :image_name => "bioblitz"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "sea_turtles",
+    :desc_key => "okaloosa_sea_turtles",
+    :image_name => "bioblitz"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "urban_habitat_adaptations",
+    :desc_key => "okaloosa_urban_adapt",
+    :image_name => "bioblitz"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "urban_habitats",
+    :desc_key => "okaloosa_urban",
+    :image_name => "bioblitz"
+  },
   ]
 
   def all_decks

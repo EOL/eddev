8e9a296b510d9961df53d3a7c0121002f724e952
Michael Vitale <mmvitale@gmail.com>
2016-12-21 16:08:49 -0500

    link download buttons to file path in deck specification'

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -3,121 +3,141 @@ module CardsHelper
   {
     :title_key  => "aquatic",
     :desc_key   => "aquatic",
-    :image_name => "aquatic_insects"
+    :image_name => "aquatic_insects",
+    :file_name => "not_found"
   },
   {
     :title_key => "bioblitz",
     :sub_key => "common_animal_groups",
     :desc_key => "bioblitz_common",
-    :image_name => "bioblitz_common"
+    :image_name => "bioblitz_common",
+    :file_name => "not_found"
   },
   {
     :title_key => "new_england",
     :sub_key => "bh_rocky_inter",
     :desc_key => "ne_bh_rocky",
-    :image_name => "new_england_rocky"
+    :image_name => "new_england_rocky",
+    :file_name => "not_found"
   },
   {
     :title_key => "earthwatch",
     :sub_key => "california_urban_tree",
     :desc_key => "earthwatch_cali_tree",
-    :image_name => "earthwatch"
+    :image_name => "earthwatch",
+    :file_name => "not_found"
   },
   {
     :title_key  => "new_england",
     :sub_key    => "ecocolumn",
     :desc_key   => "ecocolumn",
-    :image_name => "ecocolumn"
+    :image_name => "ecocolumn",
+    :file_name => "not_found"
   },
   {
     :title_key => "new_england",
     :sub_key => "urban_habitat_adaptations",
     :desc_key => "ne_urban",
-    :image_name => "ne_urban"
+    :image_name => "ne_urban",
+    :file_name => "not_found"
   },
   {
     :title_key => "new_england",
     :sub_key => "vernal_pools",
     :desc_key => "ne_vernal",
-    :image_name => "ne_vernal"
+    :image_name => "ne_vernal",
+    :file_name => "not_found"
   },
   {
     :title_key => "new_england",
     :sub_key => "wright_farm",
     :desc_key => "ne_wright",
-    :image_name => "ne_wright"
+    :image_name => "ne_wright",
+    :file_name => "not_found"
   },
   {
     :title_key => "okaloosa",
     :sub_key => "urban_habitats",
     :desc_key => "okaloosa_urban",
-    :image_name => "okaloosa_urban"
+    :image_name => "okaloosa_urban",
+    :file_name => "not_found"
   },
   {
     :title_key => "okaloosa",
     :sub_key => "pines_sandhill",
     :desc_key => "okaloosa_pines",
-    :image_name => "okaloosa_pine"
+    :image_name => "okaloosa_pine",
+    :file_name => "not_found"
   },
   {
     :title_key => "okaloosa",
     :sub_key => "freshwater_forested",
     :desc_key => "okaloosa_freshwater",
-    :image_name => "okaloosa_freshwater"
+    :image_name => "okaloosa_freshwater",
+    :file_name => "not_found"
   },
   {
     :title_key => "okaloosa",
     :sub_key => "hardwood_forested",
     :desc_key => "okaloosa_hardwood",
-    :image_name => "okaloosa_hardwood"
+    :image_name => "okaloosa_hardwood",
+    :file_name => "not_found"
   },
   {
     :title_key => "okaloosa",
     :sub_key => "coastal_uplands",
     :desc_key => "okaloosa_coastal",
-    :image_name => "okaloosa_coastal"
+    :image_name => "okaloosa_coastal",
+    :file_name => "not_found"
   },
   {
     :title_key => "okaloosa",
     :sub_key => "biodiversity",
     :desc_key => "okaloosa_bio",
-    :image_name => "okaloosa_bio"
+    :image_name => "okaloosa_bio",
+    :file_name => "not_found"
   },
   {
     :title_key => "okaloosa",
     :sub_key => "common_mammals",
     :desc_key => "okaloosa_common_mammals",
-    :image_name => "okaloosa_mammals"
+    :image_name => "okaloosa_mammals",
+    :file_name => "not_found"
   },
   {
     :title_key => "okaloosa",
     :sub_key => "common_birds",
     :desc_key => "okaloosa_common_birds",
-    :image_name => "okaloosa_birds"
+    :image_name => "okaloosa_birds",
+    :file_name => "not_found"
   },
   {
     :title_key => "okaloosa",
     :sub_key => "emammal_camera",
     :desc_key => "okaloosa_emammal",
-    :image_name => "okaloosa_emammal"
+    :image_name => "okaloosa_emammal",
+    :file_name => "not_found"
   },
   {
     :title_key => "okaloosa",
     :sub_key => "adapt",
     :desc_key => "okaloosa_adapt",
-    :image_name => "okaloosa_adapt"
+    :image_name => "okaloosa_adapt",
+    :file_name => "not_found"
   },
   {
     :title_key => "okaloosa",
     :sub_key => "sea_turtles",
     :desc_key => "okaloosa_sea_turtles",
-    :image_name => "okaloosa_turtles"
+    :image_name => "okaloosa_turtles",
+    :file_name => "not_found"
   },
   {
     :title_key => "okaloosa",
     :sub_key => "common_plants",
     :desc_key => "okaloosa_common_plants",
-    :image_name => "okaloosa_plants" 
+    :image_name => "okaloosa_plants",
+    :file_name => "not_found" 
   },
   ]
 

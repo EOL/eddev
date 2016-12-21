9ae21e4a1b747371325a34e8bbcba3122bbc89c1
Michael Vitale <mmvitale@gmail.com>
2016-12-21 15:23:57 -0500

    update title cards and descriptions (first pass)

--- app/assets/javascripts/generator.js
+++ app/assets/javascripts/generator.js
@@ -1,68 +1,85 @@
 var template = window.Trait;
 
-onFontsLoaded(function() {
-  var $canvas = $('#Canvas'),
-      canvas = $canvas[0],
-      ctx = canvas.getContext('2d'),
-      leatherbackImg = $('#LeatherbackPhoto')[0];
+window.CardGenerator = {};
 
-  var dragStart = false;
-
-  var data = {
-    'commonName': 'Leatherback Sea Turtle',
-    'sciName': 'Dermocheyls coriacea',
-    'mainPhoto': {
-      'image': leatherbackImg,
-      'sx': 0,
-      'sy': 0,
-      'sWidth': 500
-    }
-  };
-
-  ctx.canvas.width = template.width();
-  ctx.canvas.height = template.height();
-
-  template.draw(canvas, data);
-
-  $canvas.mousedown(function(e) {
-    var mouseX = e.pageX - this.offsetLeft;
-    var mouseY = e.pageY - this.offsetTop;
+(function() {
+  var exports = window.CardGenerator;
+  var data = {};
 
 
-    $.each(template.moveable(), function(i, item) {
-      if (mouseX >= item['topLeft']['x'] && mouseX <= item['bottomRight']['x'] && mouseY >= item['topLeft']['y'] && mouseY <= item['bottomRight']['y']) {
-        dragStart = {
-          'x': mouseX,
-          'y': mouseY,
-          'item': item['name']
-        };
+  function dumpData() {
+    console.log(JSON.stringify(data));
+  }
+  exports.dumpData = dumpData;
 
-        return false;
+  onFontsLoaded(function() {
+    var $canvas = $('#Canvas'),
+        canvas = $canvas[0],
+        ctx = canvas.getContext('2d'),
+        leatherbackImg = $('#LeatherbackPhoto')[0];
+
+    data = {
+      'commonName': 'Leatherback Sea Turtle',
+      'sciName': 'Dermocheyls coriacea',
+      'mainPhoto': {
+        'image': leatherbackImg,
+        'sx': 0,
+        'sy': 0,
+        'sWidth': 500
       }
-    });
-  });
+    };
 
-  function updateDrag(e) {
-    if (!dragStart) {
-      return;
-    }
+    var dragState = false;
 
-    var mouseX = e.pageX - this.offsetLeft;
-    var mouseY = e.pageY - this.offsetTop;
-
-    var dx = mouseX - dragStart['x'];
-    var dy = mouseY - dragStart['y'];
-
-    data[dragStart['item']]['sx'] -= dx;
-    data[dragStart['item']]['sy'] -= dy;
+    ctx.canvas.width = template.width();
+    ctx.canvas.height = template.height();
 
     template.draw(canvas, data);
-  }
 
-  $canvas.on('mousemove', updateDrag);
-  $canvas.mouseup(function(e) {
-    updateDrag(e);
-    dragStart = false;
+    $canvas.mousedown(function(e) {
+      var mouseX = e.pageX - this.offsetLeft;
+      var mouseY = e.pageY - this.offsetTop;
+
+
+      $.each(template.moveable(), function(i, item) {
+        if (mouseX >= item['topLeft']['x'] && 
+            mouseX <= item['bottomRight']['x'] && 
+            mouseY >= item['topLeft']['y'] && 
+            mouseY <= item['bottomRight']['y']) {
+          dragState = {
+            'x': mouseX,
+            'y': mouseY,
+            'item': item['name']
+          };
+
+          return false;
+        }
+      });
+    });
+
+    function updateDrag(e) {
+      if (!dragState) {
+        return;
+      }
+
+      var mouseX = e.pageX - this.offsetLeft;
+      var mouseY = e.pageY - this.offsetTop;
+
+      var dx = mouseX - dragState['x'];
+      var dy = mouseY - dragState['y'];
+
+      data[dragState['item']]['sx'] -= dx;
+      data[dragState['item']]['sy'] -= dy;
+
+      dragState['x'] = mouseX;
+      dragState['y'] = mouseY;
+
+      template.draw(canvas, data);
+    }
+
+    $canvas.on('mousemove', updateDrag);
+    $canvas.mouseup(function(e) {
+      dragState = false;
+    });
   });
-});
-
+})();

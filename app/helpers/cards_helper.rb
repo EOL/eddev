2eb69bb16cd83bc29b9a40f1648aa6fb09ae5ba5
Michael Vitale <mmvitale@gmail.com>
2016-12-24 15:17:48 -0500

    update title card images, order decks alphabetically for display

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -14,18 +14,18 @@ module CardsHelper
     :file_name => "bioblitz"
   },
   {
-    :title_key => "new_england",
-    :sub_key => "bh_rocky_inter",
-    :desc_key => "ne_bh_rocky",
-    :image_name => "new_england_rocky",
-    :file_name => "ne_rocky_intertidal"
-  },
-  {
     :title_key => "earthwatch",
     :sub_key => "california_urban_tree",
     :desc_key => "earthwatch_cali_tree",
     :image_name => "earthwatch",
     :file_name => "earthwatch_trees"
+  },
+  {
+    :title_key => "new_england",
+    :sub_key => "bh_rocky_inter",
+    :desc_key => "ne_bh_rocky",
+    :image_name => "new_england_rocky",
+    :file_name => "ne_rocky_intertidal"
   },
   {
     :title_key  => "new_england",
@@ -57,17 +57,38 @@ module CardsHelper
 #  },
   {
     :title_key => "okaloosa",
-    :sub_key => "urban_habitats",
-    :desc_key => "okaloosa_urban",
-    :image_name => "okaloosa_urban",
-    :file_name => "okaloosa_urban"
+    :sub_key => "adaptations",
+    :desc_key => "okaloosa_adapt",
+    :image_name => "okaloosa_adapt",
+    :file_name => "okaloosa_adaptations"
   },
   {
     :title_key => "okaloosa",
-    :sub_key => "pines_sandhill",
-    :desc_key => "okaloosa_pines",
-    :image_name => "okaloosa_pine",
-    :file_name => "okaloosa_pines_sandhill"
+    :sub_key => "biodiversity",
+    :desc_key => "okaloosa_bio",
+    :image_name => "okaloosa_bio",
+    :file_name => "okaloosa_biodiversity"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "coastal_uplands",
+    :desc_key => "okaloosa_coastal",
+    :image_name => "okaloosa_coastal",
+    :file_name => "okaloosa_coastal"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "common_mammals",
+    :desc_key => "okaloosa_common_mammals",
+    :image_name => "okaloosa_mammals",
+    :file_name => "okaloosa_mammals"
+  },
+  {
+    :title_key => "okaloosa",
+    :sub_key => "common_plants",
+    :desc_key => "okaloosa_common_plants",
+    :image_name => "okaloosa_plants",
+    :file_name => "okaloosa_plants" 
   },
   {
     :title_key => "okaloosa",
@@ -85,24 +106,24 @@ module CardsHelper
   },
   {
     :title_key => "okaloosa",
-    :sub_key => "coastal_uplands",
-    :desc_key => "okaloosa_coastal",
-    :image_name => "okaloosa_coastal",
-    :file_name => "okaloosa_coastal"
+    :sub_key => "pines_sandhill",
+    :desc_key => "okaloosa_pines",
+    :image_name => "okaloosa_pine",
+    :file_name => "okaloosa_pines_sandhill"
   },
   {
     :title_key => "okaloosa",
-    :sub_key => "biodiversity",
-    :desc_key => "okaloosa_bio",
-    :image_name => "okaloosa_bio",
-    :file_name => "okaloosa_biodiversity"
+    :sub_key => "sea_turtles",
+    :desc_key => "okaloosa_sea_turtles",
+    :image_name => "okaloosa_turtles",
+    :file_name => "okaloosa_sea_turtles"
   },
   {
     :title_key => "okaloosa",
-    :sub_key => "common_mammals",
-    :desc_key => "okaloosa_common_mammals",
-    :image_name => "okaloosa_mammals",
-    :file_name => "okaloosa_mammals"
+    :sub_key => "urban_habitats",
+    :desc_key => "okaloosa_urban",
+    :image_name => "okaloosa_urban",
+    :file_name => "okaloosa_urban"
   },
 #  {
 #    :title_key => "okaloosa",
@@ -118,27 +139,6 @@ module CardsHelper
 #    :image_name => "okaloosa_emammal",
 #    :file_name => "not_found"
 #  },
-  {
-    :title_key => "okaloosa",
-    :sub_key => "adaptations",
-    :desc_key => "okaloosa_adapt",
-    :image_name => "okaloosa_adapt",
-    :file_name => "okaloosa_adaptations"
-  },
-  {
-    :title_key => "okaloosa",
-    :sub_key => "sea_turtles",
-    :desc_key => "okaloosa_sea_turtles",
-    :image_name => "okaloosa_turtles",
-    :file_name => "okaloosa_sea_turtles"
-  },
-  {
-    :title_key => "okaloosa",
-    :sub_key => "common_plants",
-    :desc_key => "okaloosa_common_plants",
-    :image_name => "okaloosa_plants",
-    :file_name => "okaloosa_plants" 
-  },
   ]
 
   def all_decks

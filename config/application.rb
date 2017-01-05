2076de73c6df0e7f6a9a9300b42f31724c25059d
Michael Vitale <mmvitale@gmail.com>
2017-01-05 11:43:42 -0500

    rename ecocolumn file, update application.rb

--- config/application.rb
+++ config/application.rb
@@ -34,15 +34,15 @@ module Eddev
     config.x.legacy_password_salt = Figaro.env.legacy_password_salt
     
     # mail config
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address:              "smtp.gmail.com",
-      port:                 587,
-      user_name:            ENV["email_user_name"],
-      password:             ENV["email_password"],
-      authentication:       "plain",
-      enable_starttls_auto: true,
-    }
+#    config.action_mailer.delivery_method = :smtp
+#    config.action_mailer.smtp_settings = {
+#      address:              "smtp.gmail.com",
+#      port:                 587,
+#      user_name:            ENV["email_user_name"],
+#      password:             ENV["email_password"],
+#      authentication:       "plain",
+#      enable_starttls_auto: true,
+#    }
 
     # enable gzip compression
     config.middleware.use Rack::Deflater

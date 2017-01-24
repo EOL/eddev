0bb4cc7e8ee855ecb9379d5e3f47e31af07f166a
Michael Vitale <mmvitale@gmail.com>
2017-01-24 13:06:01 -0500

    Lesson plan perk seeding etc

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,44 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-License.create_with(code: "CC BY").find_or_create_by!(translation_key: "attrib")
-License.create_with(code: "CC BY-SA").find_or_create_by!(translation_key: "attrib_share")
-License.create_with(code: "CC BY-NC").find_or_create_by!(translation_key: "attrib_noncom")
-License.create_with(code: "CC BY-NC-SA").find_or_create_by!(translation_key: "attrib_noncom_share")
+#License.create_with(code: "CC BY").find_or_create_by!(translation_key: "attrib")
+#License.create_with(code: "CC BY-SA").find_or_create_by!(translation_key: "attrib_share")
+#License.create_with(code: "CC BY-NC").find_or_create_by!(translation_key: "attrib_noncom")
+#License.create_with(code: "CC BY-NC-SA").find_or_create_by!(translation_key: "attrib_noncom_share")
+#
+#SinglePageContentModel.find_or_create_by!(name: "tinymce_test")
+#
+## Basic/admin user accounts (development only)
+#case Rails.env
+#when "development"
+#  User.create_with(full_name: "Basic User",
+#                   email: "basicuser@fakeemail.com",
+#                   password: "pass1234",
+#                   password_confirmation: "pass1234",
+#                   active: true)
+#      .find_or_create_by!(user_name: "basic")
+#
+#  User.create_with(full_name: "Admin User",
+#                   email: "adminuser@fakeemail.com",
+#                   password: "pass1234",
+#                   password_confirmation: "pass1234",
+#                   active: true,
+#                   role: 'admin')
+#      .find_or_create_by!(user_name: "admin")
+#end
 
-SinglePageContentModel.find_or_create_by!(name: "tinymce_test")
-
-# Basic/admin user accounts (development only)
-case Rails.env
-when "development"
-  User.create_with(full_name: "Basic User",
-                   email: "basicuser@fakeemail.com",
-                   password: "pass1234",
-                   password_confirmation: "pass1234",
-                   active: true)
-      .find_or_create_by!(user_name: "basic")
-
-  User.create_with(full_name: "Admin User",
-                   email: "adminuser@fakeemail.com",
-                   password: "pass1234",
-                   password_confirmation: "pass1234",
-                   active: true,
-                   role: 'admin')
-      .find_or_create_by!(user_name: "admin")
-end
+LessonPlanPerk.delete_all
+LessonPlanPerk.create([
+  {
+    :name_key => "species_cards",
+    :icon_name => "card",
+  },
+  {
+    :name_key => "bio_stats",
+    :icon_name => "stats",
+  },
+  {
+    :name_key => "field_work",
+    :icon_name => "tree",
+  }
+])

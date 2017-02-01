eed1bb2f33d11b4335b1d78e0ada5c47082f0cfd
Michael Vitale <mmvitale@gmail.com>
2017-02-01 14:27:14 -0500

    not all decks have subtitles

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,6 +1,5 @@
 class Deck < ActiveRecord::Base
   validates_presence_of :title_key
-  validates_presence_of :subtitle_key
   validates_presence_of :desc_key
   validates_presence_of :file_name
   validates_uniqueness_of :file_name

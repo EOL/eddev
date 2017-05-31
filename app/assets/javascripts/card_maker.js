d45c88669e39adab4d1159bf555d4556989c1bec
Michael Vitale <mmvitale@gmail.com>
2017-05-31 11:40:53 -0400

    preview box + slide animation

--- app/assets/javascripts/card_maker.js
+++ app/assets/javascripts/card_maker.js
@@ -1,2 +1,30 @@
 //= require template_renderer/template-renderer
-//= require generator
+
+$(function() {
+  function setPreviewTopMargin() {
+    var scrollTop = $(this).scrollTop()
+      , $preview = $('#Preview')
+      , $col = $('#LeftCol')
+      , $colHead = $('#LeftColHeadBox')
+      , colOffsetTop = $col.offset().top
+      , colViewOffset = colOffsetTop - scrollTop
+      , colHeadHeight = $colHead.outerHeight()
+      , colHeight = $col.outerHeight()
+      , innerColViewOffset = colViewOffset + colHeadHeight
+      , previewHeight = $preview.outerHeight()
+      , top
+      ;
+
+    if (innerColViewOffset < 0) {
+      top = Math.min(colViewOffset * -1, colHeight - previewHeight);
+    } else {
+      top = colHeadHeight;
+    }
+
+    $preview.animate({
+      top: top
+    }, 50);
+  }
+
+  $(document).scroll(setPreviewTopMargin);
+});

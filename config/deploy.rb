c63472eebda6206842269f50825b7c4fc73f09f7
Michael Vitale <mmvitale@gmail.com>
2017-05-31 16:07:28 -0400

    update Capistrano settings for newer ruby

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.1'
+lock '>=3.4.1'
 
 set :application, 'eddev'
 set :repo_url, 'git@github.com:EOL/eddev.git'
@@ -11,9 +11,6 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/var/www/eddev'
-
-# Default value for :scm is :git
-set :scm, :git
 
 # Default value for :format is :pretty
 set :format, :pretty

9a1e15c4b9ac2bc84514bf2e6126914682598e9d
Michael Vitale <mmvitale@gmail.com>
2018-02-07 13:52:23 -0500

    editor icons

--- app/javascript/card-maker/components/save-exit-btns.jsx
+++ app/javascript/card-maker/components/save-exit-btns.jsx
@@ -29,12 +29,15 @@ function saveExitBtns(props) {
   return (
     <div className='preview-btns'>
       <div className='btn close' onClick={props.handleClose}>
+        <i className='cm-icon-cancel' />
         <div className='btn-txt'>{I18n.t('react.card_maker.close')}</div>
       </div>
       <div className={saveExitClass} onClick={handleSaveAndExit}>
+        <i className='cm-icon-save-exit' />
         <div className='btn-txt'>{I18n.t('react.card_maker.save_+_exit')}</div>
       </div>
       <div className={saveClass} onClick={handleSave}>
+        <i className='cm-icon-save-quick' />
         <div className='btn-txt'>{I18n.t('react.card_maker.quick_save')}</div>
       </div>
     </div>

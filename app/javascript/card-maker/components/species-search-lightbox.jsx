66333ca67e3db24e7f0cdc03a2862015ff8a9726
Michael Vitale <mmvitale@gmail.com>
2018-05-08 12:24:57 -0400

    template tab basic layout

--- app/javascript/card-maker/components/species-search-lightbox.jsx
+++ app/javascript/card-maker/components/species-search-lightbox.jsx
@@ -2,17 +2,35 @@ import React from 'react'
 import ReactModal from 'react-modal'
 
 import SpeciesSearch from './species-search'
+import TemplateSelect from './template-select'
 import UserResourceFilter from './user-resource-filter'
 
 import styles from 'stylesheets/card_maker/card_manager'
 
+var cleanState = {
+  screen: 'search'
+};
+
 class SpeciesSearchLightbox extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = cleanState;
   }
 
   handleRequestClose = () => {
     this.props.handleClose();
+    this.setState(cleanState);
+  }
+
+  screenComponent = () => {
+    if (this.state.screen === 'search') {
+      return <SpeciesSearch {...this.props} />
+    } else if (this.state.screen = 'template') {
+      return <TemplateSelect />
+    } else {
+      throw new TypeError('unsupported "screen" state value: ' + this.state.screen);
+    }
   }
 
   render() {
@@ -41,7 +59,14 @@ class SpeciesSearchLightbox extends React.Component {
             handleSelect={this.props.handleDeckSelect}
           />
         </div>
-        <SpeciesSearch {...this.props} />
+        {this.screenComponent()}
+        {this.state.screen === 'search' && (
+          <button 
+            className={styles.selectTemplateBtn} 
+            type='button'
+            onClick={() => this.setState({ screen: 'template' })}
+          >{I18n.t('react.card_maker.click_choose_card_type')}</button>
+        )}
       </ReactModal>
     )
   }
---
+++ app/javascript/card-maker/components/template-select.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import styles from 'stylesheets/card_maker/card_manager'
+import bioCard from 'images/card_maker/sample_cards/biodiversity.png'
+
+class TemplateSelect extends React.Component {
+  render() {
+    return (
+      <ul className={styles.cardTemplates}>
+        <li className={styles.cardTemplate}>
+          <img src={bioCard} />
+          <div>{I18n.t('react.card_maker.bio_card')}</div>
+          <button
+            className={[styles.createBtn, styles.createBtnTempl].join(' ')}
+            type='button'
+          >{I18n.t('react.card_maker.create')}</button>
+        </li>
+      </ul>
+    );
+  }
+}
+
+export default TemplateSelect;

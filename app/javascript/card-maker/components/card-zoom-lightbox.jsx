f2ceef3ea3b8fb1c39c0df72172362596b59e91e
Michael Vitale <mmvitale@gmail.com>
2018-07-17 16:44:59 -0400

    add close button, fix styling

--- app/javascript/card-maker/components/card-zoom-lightbox.jsx
+++ app/javascript/card-maker/components/card-zoom-lightbox.jsx
@@ -1,5 +1,5 @@
 import React from 'react' // Actually necessary to fix transpiled code :(
-import ReactModal from 'react-modal'
+import CloseButtonModal from './close-button-modal'
 import LoadingSpinnerImage from './loading-spinner-image'
 import {cardMakerUrl, hiResCardImageUrl} from 'lib/card-maker/url-helper'
 import styles from 'stylesheets/card_maker/card_manager'
@@ -10,31 +10,35 @@ function handlePngClick(cardId) {
 
 function CardZoomLightbox(props) {
   return (
-    <ReactModal
+    <CloseButtonModal
       isOpen={props.card != null}
       contentLabel={I18n.t('react.card_maker.card_preview_lightbox')}
       parentSelector={() => {return document.getElementById('Page')}}
       overlayClassName={[styles.lOverlay, styles.lOverlayManager].join(' ')}
-      className={[styles.lightbox, styles.lightboxZoom].join(' ')}
+      className={[styles.lightbox, styles.lightboxNoGlow].join(' ')}
       bodyOpenClassName='noscroll'
       onRequestClose={props.handleRequestClose}
+      shouldCloseOnOverlayClick={false}
+      fullScreenOverlay={true}
     >
-      <i 
-        className={`fa fa-angle-left fa-4x ${styles.zoomArrow} ${styles.zoomArrowLeft}`} 
-        onClick={props.requestPrev}
-      />
-      {props.card != null &&
-        <div className={styles.imageZoom}>
-          <LoadingSpinnerImage src={hiResCardImageUrl(props.card)} />
-        </div>
-      }
-      <button onClick={() => handlePngClick(props.card.id)}>{I18n.t('react.card_maker.download_png')}</button>
-      <i 
-        className={`fa fa-angle-right fa-4x ${styles.zoomArrow} ${styles.zoomArrowRight}`}
-        onClick={props.requestNext}
-      />
+      <div className={styles.cardZoom}>
+        <i 
+          className={`fa fa-angle-left fa-4x ${styles.zoomArrow} ${styles.zoomArrowLeft}`} 
+          onClick={props.requestPrev}
+        />
+        {props.card != null &&
+          <div className={styles.imageZoom}>
+            <LoadingSpinnerImage src={hiResCardImageUrl(props.card)} />
+          </div>
+        }
+        <button onClick={() => handlePngClick(props.card.id)}>{I18n.t('react.card_maker.download_png')}</button>
+        <i 
+          className={`fa fa-angle-right fa-4x ${styles.zoomArrow} ${styles.zoomArrowRight}`}
+          onClick={props.requestNext}
+        />
+      </div>
 
-    </ReactModal>
+    </CloseButtonModal>
   );
 }
 

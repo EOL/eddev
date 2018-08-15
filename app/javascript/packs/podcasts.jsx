b44410aa9840c302c1b766a00176db997be27f61
Michael Vitale <mmvitale@gmail.com>
2018-08-15 12:30:09 -0400

    reconfigure react to use ujs

--- app/javascript/packs/podcasts.jsx
+++ app/javascript/packs/podcasts.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom'
 
 import Podcasts from 'podcasts/components/podcasts'
 
+  /*
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Podcasts />,
     document.getElementById('PodcastsReact'),
   );
 });
+*/
 

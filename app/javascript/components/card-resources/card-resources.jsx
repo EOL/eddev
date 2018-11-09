fd45a58bdebf727cb6690573abf133813f22126a
Michael Vitale <mmvitale@gmail.com>
2018-11-09 09:58:27 -0500

    start implementation

--- app/javascript/components/card-resources/card-resources.jsx
+++ app/javascript/components/card-resources/card-resources.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import styles from 'stylesheets/card_resources'
 
 class CardResources extends React.Component {
   render() {
     return (
-      <div>Hello, Card Resource World!</div>
+      <div className={styles.cardResources}>
+        <div className={styles.boxes}>
+          <div className={styles.boxWelcome}>
+            <h2>Welcome to the EOL Card Maker</h2>
+            <p>This whole project is an over-engineered waste of time this whole project is an over-engineered waste of time etc etc.</p>
+          </div>
+        </div>
+      </div>
     );
   }
 }

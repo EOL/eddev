256c2471ef5cb94890e46bed465f2406b8a84fe7
Michael Vitale <mmvitale@gmail.com>
2019-01-14 11:56:10 -0500

    standardize intro section styling

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,6 @@
 class CardsController < ApplicationController
   def index
-    @hero_image_partial = "cards/banner"
+    @hero_image_partial = "shared/hero_slim"
 
     @food_chains_rummy = LessonPlan.find_by_human_name!("food_chains_rummy_9_12")
     @go_adapt = LessonPlan.find_by_human_name!("go_adapt_2_5")

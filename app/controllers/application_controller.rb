c83bef500fc478d026a3d4ff04cdb503b0096c28
Michael Vitale <mmvitale@gmail.com>
2019-01-22 15:57:22 -0500

    show message if unconfirmed user signs in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,10 +22,10 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in_user
-    nil
-
     if session[:user_id]
       @logged_in_user ||= User.find_by(:id => session[:user_id])
+    else
+      nil
     end
   end
   helper_method :logged_in_user
@@ -43,8 +43,11 @@ class ApplicationController < ActionController::Base
     def log_in(user_name, password)
       @logged_in_user = User.find_by(user_name: user_name).try(:authenticate, password)
 
-      if @logged_in_user && @logged_in_user.confirmed?
-        session[:user_id] = @logged_in_user.id
+      if @logged_in_user
+        if @logged_in_user.confirmed?
+          session[:user_id] = @logged_in_user.id
+        end
+
         @logged_in_user
       else
         nil

0b8aefbf3d56f516b785eb4b91fb4699462347ac
Michael Vitale <mmvitale@gmail.com>
2019-01-22 17:12:03 -0500

    big controller/view cleanup

--- app/controllers/i18n_test_controller.rb
+++
@@ -1,9 +0,0 @@
-class I18nTestController < ApplicationController
-  def index
-    if params[:user_lang] && logged_in_user
-      logged_in_user.update!(locale: params[:user_lang]) 
-      I18n.locale = params[:user_lang]
-      redirect_to "/i18ntest"
-    end
-  end
-end
--- app/views/gallery_photos/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @gallery_photo, :id, :created_at, :updated_at
